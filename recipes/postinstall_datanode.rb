5f88f7cc59db85dac6405ee0657c3efa6e884100
lohitch <lohitch@gmail.com>
2016-02-19 20:25:00 +0530

    Removed postinstall_datanode recipe

--- recipes/postinstall_datanode.rb
+++
@@ -1,9 +0,0 @@
-include_recipe 'hadoop::hadoop_hdfs_datanode'
-
-ruby_block 'service-hadoop-hdfs-datanode-start-and-enable' do
-  block do
-    %w(enable start).each do |action|
-      resources('service[hadoop-hdfs-datanode]').run_action(action.to_sym)
-    end
-  end
-end
