877e58039d77fae7215c30a53c101c3e4ad83f33
GG <ggn@GGs-MacBook-Pro.local>
2016-02-22 12:51:14 +0530

    over the name node ip atribute

---
+++ attributes/default.rb
@@ -0,0 +1,5 @@
+###
+# core-site.xml settings. Overridden...
+###
+default['hadoop-datanode']['namenodeIP'] = #{node['fqdn']}
+override['hadoop']['core_site']['fs.defaultFS'] = "hdfs://node['hadoop-datanode']['namenodeIP']"
